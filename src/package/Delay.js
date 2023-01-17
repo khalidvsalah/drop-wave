9f25bef113d991e175751a10606e23fe44eb43b5
khalidsalah1522 <khalidsalah1522@gmail.com>
2023-01-17 15:59:00 +0200

    change Delay function to a Class

--- src/package/Delay.js
+++ src/package/Delay.js
@@ -1,5 +1,14 @@
 import _F from "./Frame.js";
 
-export default function Delay({ delay, cb }) {
-  return _F.push({ completed: cb, d: delay });
+export default class Delay {
+  constructor({ delay, cb }) {
+    this.delay = delay;
+    this.cb = cb;
+    this.index = _F.push({ completed: cb, d: delay });
+  }
+
+  kill() {
+    _F.kill(this.index);
+    _F.push({ completed: this.cb, d: 0 });
+  }
 }
