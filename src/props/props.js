10a1bea6cc9b20ed3785bb5e31b41c29f3ef02fc
khalidsalah1522 <khalidsalah1522@gmail.com>
2023-01-26 11:55:59 +0200

    Add Lerp prop

--- src/props/props.js
+++ src/props/props.js
@@ -4,22 +4,27 @@ const props = {
       start: x[0],
       end: x[1],
       unit: x[2] ? x[2] : "%",
+      lerp: x[1] - x[0],
     };
 
     var yV = {
       start: y[0],
       end: y[1],
       unit: y[2] ? y[2] : "%",
+      lerp: y[1] - y[0],
     };
 
     if (x && y) {
       return (e) =>
-        `translate3d(${xV.end * e}${xV.unit}, ${yV.end * e}${yV.unit}, 0)`;
+        `translate3d(
+          ${xV.start + xV.lerp * e}${xV.unit}, 
+          ${yV.start + yV.lerp * e}${yV.unit},
+          0)`;
     } else {
       if (x) {
-        return (e) => `translateX(${xV.end * e}${xV.unit})`;
+        return (e) => `translateX(${xV.start + xV.lerp * e}${xV.unit})`;
       } else if (y) {
-        return (e) => `translateY(${yV.end * e}${yV.unit})`;
+        return (e) => `translateY(${yV.start + yV.lerp * e}${yV.unit})`;
       }
     }
   },
