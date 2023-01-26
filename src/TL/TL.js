2cfbc2c683b3778ec00119242a9867eaac8eaf31
khalidsalah1522 <khalidsalah1522@gmail.com>
2023-01-26 15:12:08 +0200

    Add a third arg for ± time

--- src/TL/TL.js
+++ src/TL/TL.js
@@ -6,26 +6,17 @@ class TL {
     this.items = [];
   }
 
-  /*
-      to(element, o, less or more (time)) {
-      for(let i = 0; i < this.items.length; i++) {
-        if(i === 0) {
-          this.items.push(new Tr(element, {o}));
-        }else {
-          this.items.push(new Tr(element, {...o, delay: o.delay + this.items[i - 1].delay}));
-        }
-      }
-      }
-    */
-
-  to(element, o) {
+  to(element, o, time = 0) {
     if (this.count === 0) {
       this.items.push(new T(element, o));
     } else {
       this.items.push(
         new T(element, {
           ...o,
-          delay: this.items[this.count - 1].del + this.items[this.count - 1].d,
+          delay:
+            this.items[this.count - 1].del +
+            this.items[this.count - 1].d +
+            time,
         })
       );
     }
