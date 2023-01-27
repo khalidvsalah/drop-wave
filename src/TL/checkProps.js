221dd2307bb2049d7890844b219f65c8f0ea804d
khalidsalah1522 <khalidsalah1522@gmail.com>
2023-01-27 21:34:27 +0200

    pass css-property object

--- src/TL/checkProps.js
+++ src/TL/checkProps.js
@@ -3,16 +3,20 @@ import props from "./props.js";
 
 export default function checkProps() {
   this.results = [];
+  var n = window.getComputedStyle(this.selector[0]);
 
   if (A.Has(this.props, "x") || A.Has(this.props, "y")) {
     var x = A.Has(this.props, "x") && this.props["x"];
     var y = A.Has(this.props, "y") && this.props["y"];
-    this.results.push({ name: "transform", cb: props["transform"](x, y) });
+    this.results.push({
+      name: "transform",
+      cb: props["transform"](x, y, n),
+    });
   }
 
   if (A.Has(this.props, "opacity")) {
     var o = this.props["opacity"];
-    this.results.push({ name: "opacity", cb: props["opacity"](o) });
+    this.results.push({ name: "opacity", cb: props["opacity"](o, n) });
   }
 
   if (A.Has(this.props, "pointer")) {
