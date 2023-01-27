1e9fb6148acf05c1dd34758e6b2f7e2e09a8dbc3
khalidsalah1522 <khalidsalah1522@gmail.com>
2023-01-27 21:44:58 +0200

    convert % to px

--- src/TL/props.js
+++ src/TL/props.js
@@ -7,35 +7,39 @@ const props = {
     if (tR) {
       xV = {
         s: +tR[4],
-        e: x ? x[1] : 0,
+        e: x ? (x[2] === "px" ? x[1] : (x[1] / 100) * parseFloat(n.width)) : 0,
+        unit: "px",
       };
 
       yV = {
         s: +tR[5],
-        e: y ? y[1] : 0,
+        e: y ? (y[2] === "px" ? y[1] : (y[1] / 100) * parseFloat(n.height)) : 0,
+        unit: "px",
       };
     } else {
       xV = {
         s: x[0],
         e: x[1],
+        unit: x[2] ? x[2] : "px",
       };
       yV = {
         s: y[0],
         e: y[1],
+        unit: y[2] ? y[2] : "px",
       };
     }
 
     xV.lerp = xV.e - xV.s;
     yV.lerp = yV.e - yV.s;
-    xV.unit = x[2] ? x[2] : "px";
-    yV.unit = y[2] ? y[2] : "px";
 
     if ((x && y) || tR) {
-      return (e) =>
-        `translate3d(
+      return (e) => {
+        console.log(yV.e, yV.lerp);
+        return `translate3d(
           ${xV.s + xV.lerp * e}${xV.unit}, 
           ${yV.s + yV.lerp * e}${yV.unit},
           0)`;
+      };
     } else {
       if (x) {
         return (e) => `translateX(${xV.s + xV.lerp * e}${xV.unit})`;
