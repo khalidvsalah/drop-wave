f9d094ba4c5ee6b7b5bde8feaf621897f02527ec
khalidsalah1522 <khalidsalah1522@gmail.com>
2023-01-28 11:45:06 +0200

    Tr => TL

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import _F from "./src/Frame.js";
 import _R from "./src/Route.js";
 import _C from "./src/Mount.js";
 import Delay from "./src/Delay.js";
-import Tr from "./src/TL/TL.js";
+import TL from "./src/TL/TL.js";
 import { Ease, Clamp, Lerp } from "./src/Math.js";
 import { PrE } from "./src/Utils.js";
 
@@ -14,7 +14,7 @@ const Ardor = {
   Clamp,
   Lerp,
   Delay,
-  Tr,
+  TL,
   PrE,
   Has: (o, p) => window.hasOwnProperty.call(o, p),
 };
