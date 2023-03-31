deb7692342635d131ca7605d8be4c46d42f3c401
khalidsalah1522 <khalidsalah1522@gmail.com>
2023-03-31 16:28:28 +0200

    refactor delay file

--- src/Animation/Delay/Delay.js
+++ src/Animation/Delay/Delay.js
@@ -7,32 +7,20 @@ export default class Delay {
     this.played = false;
   }
 
-  // kill() {
-  //   if (!this.played) {
-  //     console.warn("You need to play it before kill it.");
-  //     return;
-  //   }
-  //   Raf.kill(this.index);
-  //   this.elapsed();
-  // }
-
   play() {
     this.played = true;
     this.index = Raf.push({
       completed: this.elapsed.bind(this),
       d: this.delay,
     });
-    if (!Raf.on) {
-      Raf.play();
-    }
+
+    if (!Raf.on) Raf.play();
   }
 
   elapsed() {
     Raf.push(this.o);
 
-    if (!Raf.on) {
-      Raf.play();
-    }
+    if (!Raf.on) Raf.play();
   }
 
   remove() {
