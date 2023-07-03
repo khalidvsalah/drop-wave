72d92c2c993da19e53b3c63d6214c5da7ef93bd1
Khalid Salah <khalidsalah1522@gmail.com>
2023-07-04 00:36:17 +0300

    add horizontal ability to scroll

--- src/Core/Scroll/scroll.js
+++ src/Core/Scroll/scroll.js
@@ -1,49 +1,36 @@
-import { Sub, Tween, Bounds, Clamp, Lerp, Raf, Throttle } from "../../index";
+import { Sub, Tween, Bounds, Clamp, Lerp, Throttle } from "../../index";
 
 class Scroll {
-  constructor(
-    ele,
-    { ease = 0.1, wheelLerp = 0.1, drag = true, dragLerp = 0.05 } = {}
-  ) {
+  constructor(ele, o = {}) {
     history.scrollRestoration = "manual";
 
+    this.ease = o.ease || 0.1;
+    this.dir = o.dir ? o.dir : "y";
+    this.wheelLerp = o.wheelLerp || 0.2;
+    this.dragLerp = o.dragLerp || 0.05;
+    this.dragOn = o.drag;
+
     this.ele = ele;
-    this.lerp = { x: 0, y: 0, lerp: ease };
+    this.lerp = { x: 0, y: 0, lerp: this.ease };
 
-    this.wheel = { x: 0, y: 0, lerp: wheelLerp };
-    this.drag = { s: 0, e: 0, sp: 0, ep: 0, lerp: dragLerp, on: false };
+    this.wheel = { x: 0, y: 0, lerp: this.wheelLerp };
+    this.drag = {
+      x: { s: 0, e: 0, sp: 0, ep: 0 },
+      y: { s: 0, e: 0, sp: 0, ep: 0 },
+      lerp: this.dragLerp,
+    };
 
-    if (!Sub.check("wheel")) {
-      window.addEventListener("wheel", Sub.obs("wheel").cb);
+    if (this.dragOn) {
+      this.smousedown = Sub.add("mousedown", this.onMDown.bind(this));
+      this.smousemove = Sub.add("mousemove", this.onMM.bind(this));
+      this.smouseup = Sub.add("mouseup", this.onMU.bind(this));
     }
 
-    if (!Sub.check("resize")) {
-      window.addEventListener("resize", Sub.obs("resize").cb);
-    }
+    this.sresize = Sub.add("resize", this.Resize.bind(this));
+    this.swheel = Sub.add("wheel", this.onWheel.bind(this));
+    this.sraf = Sub.add("raf", this.raf.bind(this));
 
-    if (drag) {
-      if (!Sub.check("mousedown"))
-        window.addEventListener("mousedown", Sub.obs("mousedown").cb);
-      if (!Sub.check("mousemove"))
-        window.addEventListener("mousemove", Sub.obs("mousemove").cb);
-      if (!Sub.check("mouseup"))
-        window.addEventListener("mouseup", Sub.obs("mouseup").cb);
-
-      this.mdId = Sub.add("mousedown", this.onMDown.bind(this));
-      this.mmId = Sub.add("mousemove", this.onMM.bind(this));
-      this.muId = Sub.add("mouseup", this.onMU.bind(this));
-    }
-
-    this.Resize();
-
-    if (!Sub.check("raf")) {
-      Raf.push({ d: -1, cb: Sub.obs("raf").cb });
-    }
-
-    this.wId = Sub.add("wheel", this.onWheel.bind(this));
-    this.resizeId = Sub.add("resize", this.Resize.bind(this));
-    this.rafId = Sub.add("raf", this.raf.bind(this));
-    this.scrollId = Sub.obs("scroll");
+    this.rscroll = Sub.obs("scroll");
 
     this.all = document.getElementById("all");
     if (!this.all) {
@@ -53,50 +40,70 @@ class Scroll {
       document.body.appendChild(this.all);
     }
 
-    this.thr = new Throttle({
+    this.throttle = new Throttle({
       late: 0.3,
       cb: () => {
         this.all.style.pointerEvents = "none";
       },
     });
+
+    this.Resize();
   }
 
   onWheel(e) {
-    this.lerp.x += e.deltaX * this.lerp.lerp;
+    this.lerp.x += e.deltaY * this.lerp.lerp;
     this.lerp.y += e.deltaY * this.lerp.lerp;
 
     this.all.style.pointerEvents = "all";
-    this.thr.run();
+    this.throttle.run();
   }
 
   onMDown(e) {
-    this.drag.s = e.pageY;
-    this.drag.on = true;
+    this.drag.y.s = e.pageY;
+    this.drag.x.s = e.pageY;
+
+    this.down = true;
   }
 
   onMM(e) {
-    if (this.drag.on) {
+    if (this.down) {
       this.all.style.pointerEvents = "all";
-      this.thr.run();
+      this.throttle.run();
 
-      this.drag.prev = this.drag.e;
-      this.drag.e = e.pageY;
+      if (this.dir === "y") {
+        this.drag.y.prev = this.drag.y.e;
+        this.drag.y.e = e.pageY;
 
-      if (this.drag.d === 1) this.drag.sp = this.drag.e;
-      if (this.drag.d === -1) this.drag.ep = this.drag.e;
+        if (this.drag.y.d === 1) this.drag.y.sp = this.drag.y.e;
+        if (this.drag.y.d === -1) this.drag.y.ep = this.drag.y.e;
 
-      var diff = this.drag.e - this.drag.s;
-      this.drag.d = Math.sign(this.drag.prev - this.drag.e) * -1;
+        var diff = this.drag.y.e - this.drag.y.s;
+        this.drag.y.d = Math.sign(this.drag.y.prev - this.drag.y.e) * -1;
 
-      if (this.drag.d === -1) diff = this.drag.e - this.drag.sp;
-      if (this.drag.d === 1) diff = this.drag.e - this.drag.ep;
+        if (this.drag.y.d === -1) diff = this.drag.y.e - this.drag.y.sp;
+        if (this.drag.y.d === 1) diff = this.drag.y.e - this.drag.y.ep;
 
-      this.lerp.y = diff * -1 * this.drag.lerp + this.lerp.y;
+        this.lerp.y = diff * -1 * this.drag.lerp + this.lerp.y;
+      } else {
+        this.drag.x.prev = this.drag.x.e;
+        this.drag.x.e = e.pageX;
+
+        if (this.drag.x.d === 1) this.drag.x.sp = this.drag.x.e;
+        if (this.drag.x.d === -1) this.drag.x.ep = this.drag.x.e;
+
+        var diff = this.drag.x.e - this.drag.x.s;
+        this.drag.x.d = Math.sign(this.drag.x.prev - this.drag.x.e) * -1;
+
+        if (this.drag.x.d === -1) diff = this.drag.x.e - this.drag.x.sp;
+        if (this.drag.x.d === 1) diff = this.drag.x.e - this.drag.x.ep;
+
+        this.lerp.x = diff * -1 * this.drag.lerp + this.lerp.x;
+      }
     }
   }
 
   onMU() {
-    this.drag.on = false;
+    this.down = false;
   }
 
   add(ele, o) {
@@ -122,12 +129,8 @@ class Scroll {
   }
 
   raf() {
-    this.lerp.x = Clamp(0, this.bounds.width, this.lerp.x);
-    this.lerp.y = Clamp(
-      0,
-      this.bounds.height - window.innerHeight,
-      this.lerp.y
-    );
+    this.lerp.x = Clamp(0, this.bounds.width - innerWidth, this.lerp.x);
+    this.lerp.y = Clamp(0, this.bounds.height - innerHeight, this.lerp.y);
 
     let x = Lerp(this.wheel.x, this.lerp.x, this.wheel.lerp);
     let y = Lerp(this.wheel.y, this.lerp.y, this.wheel.lerp);
@@ -135,8 +138,13 @@ class Scroll {
     this.wheel.x = x;
     this.wheel.y = y;
 
-    this.ele.style.transform = `translateY(${-this.wheel.y}px)`;
-    this.scrollId.cb(this.wheel);
+    if (this.dir === "y") {
+      this.ele.style.transform = `translateY(${-this.wheel.y}px)`;
+    } else {
+      this.ele.style.transform = `translateX(${-this.wheel.x}px)`;
+    }
+
+    this.rscroll.cb(this.wheel);
   }
 
   Resize() {
@@ -144,13 +152,15 @@ class Scroll {
   }
 
   destroy() {
-    Sub.remove("wheel", this.wId);
-    Sub.remove("resize", this.resizeId);
-    Sub.remove("raf", this.rafId);
+    Sub.remove("wheel", this.swheel);
+    Sub.remove("resize", this.sresize);
+    Sub.remove("raf", this.sraf);
 
-    this.mdId && Sub.remove("mousedown", this.mdId);
-    this.mmId && Sub.remove("mousemove", this.mmId);
-    this.muId && Sub.remove("mouseup", this.muId);
+    if (this.dragOn) {
+      Sub.remove("mousedown", this.smousedown);
+      Sub.remove("mousemove", this.smousemove);
+      Sub.remove("mouseup", this.smouseup);
+    }
   }
 }
 
