fc6809972a6f87281368edf3bb0ef72b4702cd7e
Khalid Salah <khalidsalah1522@gmail.com>
2023-07-08 00:35:14 +0300

    remove unnessery properties

--- src/Core/Timeline/tools/props/checkProps.js
+++ src/Core/Timeline/tools/props/checkProps.js
@@ -4,11 +4,10 @@ export default function checkProps() {
   this.results = [];
 
   if (this.ps && !this.obj) {
-    let x, y, o, p, d, sx, sy, t;
+    let x, y, o, sx, sy;
 
     let ele = this.target;
-    let c = window.getComputedStyle(ele);
-    let ph = ele.parentNode.clientHeight;
+    let computed = window.getComputedStyle(ele);
 
     x = this.ps["x"] || false;
     y = this.ps["y"] || false;
@@ -16,42 +15,18 @@ export default function checkProps() {
     sx = this.ps["sx"] || false;
     sy = this.ps["sy"] || false;
 
-    t = this.ps["t"] || false;
-
     o = this.ps["opacity"] || false;
-    p = this.ps["pointer"] || false;
-    d = this.ps["display"] || false;
 
     if (x || y || sx || sy) {
       this.results.push({
         name: "transform",
-        cb: props["transform"](x, y, sx, sy, c),
+        cb: props["transform"](x, y, sx, sy, computed),
       });
     }
 
-    t &&
-      this.results.push({
-        name: "top",
-        cb: props["top"](t, c, ph),
-      });
-
-    p &&
-      this.results.push({
-        name: "pointerEvents",
-        cb: props["pointer"](p),
-      });
-
-    d &&
-      this.results.push({
-        name: "display",
-        cb: props["display"](d),
-      });
-
-    o &&
-      this.results.push({
-        name: "opacity",
-        cb: props["opacity"](o, c),
-      });
+    if (o) {
+      this.results.push({ name: "opacity", cb: props["opacity"](o, computed) });
+    }
   } else if (this.obj) {
     for (let key in this.ps) {
       let p = this.ps[key];
