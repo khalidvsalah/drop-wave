14eaf281901162b8f9140f8aa9c807d5e6e934ed
Khalid Salah <khalidsalah1522@gmail.com>
2023-07-10 17:34:25 +0300

    add iremap fun

--- src/Core/Math/Math.js
+++ src/Core/Math/Math.js
@@ -36,6 +36,7 @@ export const Ease = {
 export const Clamp = (m, x, z) => Math.min(Math.max(m, z), x);
 export const Lerp = (s, e, amt) => (1 - amt) * s + amt * e;
 export const Remap = (a, b, x) => Clamp(0, 1, (x - a) / (b - a));
+export const iRemap = (a, b, c, d, x) => Remap(a, b, x) * (d - c) + c;
 export const Round = (num, pow) => {
   var d = pow ? Math.pow(10, pow) : 100;
   return Math.round(num * d) / d;
