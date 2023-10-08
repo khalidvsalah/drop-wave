e2dbdef4a35378b3e598e590521ec06caed7e2df
Khalid Salah <khalidsalah1522@gmail.com>
2023-10-08 21:05:37 +0300

    solve trigger bug

--- src/Core/Scroll/trigger.js
+++ src/Core/Scroll/trigger.js
@@ -2,17 +2,19 @@ import { Tween, Bounds, Sub, Remap, Props } from "../../index";
 
 class Trigger {
   constructor(el, o, sub, dir) {
-    this.sub = sub;
-    this.id = Sub.add(sub, this.raf.bind(this));
+    this.iscroll = Sub.add(sub, this.raf.bind(this));
     this.iresize = Sub.add("resize", this.resize.bind(this));
 
     this.o = o;
-    this.el = el;
+    this.el = el; // starting point
 
     this.dir = dir;
     this.d = dir ? "y" : "x";
 
-    if (o.t) this.ps = Props(o.t, false, o.o.p);
+    if (!o.target) o.target = el;
+    if (o.scroll) {
+      this.ps = Props(o.target.length ? o.target[0] : o.target, false, o.o.p);
+    }
 
     this.resize();
   }
@@ -32,20 +34,29 @@ class Trigger {
     } else {
       let d = e[this.d] + this.l;
       let p = ((this.o.s || 0) / 100) * this.l;
-      if (p + this.p <= d) this.fire();
+      if (p + this.p < d) this.fire();
     }
   }
 
   fire() {
-    Tween(this.o.t || this.el, this.o.o);
+    Tween(this.o.target || this.el, this.o.o);
     this.o.cb && this.o.cb();
-    this.id.r();
+    this.iscroll.r();
   }
 
   scroll(t) {
     if (!this.in) return;
     this.o.raf && this.o.raf(t);
-    this.ps.map((p) => (this.o.t.style[p.name] = p.cb(t)));
+
+    this.ps.map((p) => {
+      if (this.o.target.length) {
+        this.o.target.forEach((t) => {
+          t.style[p.name] = p.cb(t);
+        });
+      } else {
+        this.o.target.style[p.name] = p.cb(t);
+      }
+    });
   }
 
   resize() {
