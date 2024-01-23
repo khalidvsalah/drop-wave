7ab475e10dddba8c19edce5cac3d7f2fc938e5bc
Khalid Salah <khalidsalah1522@gmail.com>
2024-01-23 22:19:10 +0200

    fix split-line function

--- src/Plugins/src/line.js
+++ src/Plugins/src/line.js
@@ -1,4 +1,6 @@
-import { computed } from '../../index';
+import { computed, iSet } from '../../index';
+
+const space = ' ';
 
 function init(div, compute) {
   document.body.append(div);
@@ -15,10 +17,6 @@ function init(div, compute) {
   div.style.letterSpacing = compute.getPropertyValue('letter-spacing');
   div.style.lineHeight = compute.getPropertyValue('line-height');
 }
-const getWidth = (node) => {
-  node.style.display = 'inline-block';
-  return node.offsetWidth;
-};
 
 function splitText(node) {
   const nodes = node.childNodes;
@@ -59,131 +57,94 @@ function filter(text) {
     }
   }
 }
+function divide(lines, output, o) {
+  if (o.words) {
+    const len = lines.words.length;
 
-function measure(node, key, line) {
-  const space = '&nbsp;';
+    if (o.ltrs) {
+      const line = lines.words.reduce((a, b, i) => {
+        let str = '';
+        for (let i = 0; i < b.length; i++) str += wrap(b[i], 3);
+        return a + wrap(str + (i == len - 1 ? '' : space), 2);
+      }, '');
+      output.push({ line: wrap(line, 1) });
+    } else {
+      const line = lines.words.reduce((a, b, i) => {
+        return a + wrap(b + (i == len - 1 ? '' : space), 2);
+      }, '');
 
-  if (node.type === 3) {
-    const word = key + space;
-    line.value += word;
-    line.words.push({ word, node: node.node });
-  } else {
-    for (let i = 0; i < key.value.length; i++) {
-      const word = key.value[i] + space;
-      line.value += word;
-      line.words.push({ word, node: node.node });
+      output.push({ line: wrap(line, 1) });
     }
   }
 }
-function lines(text, span, width) {
-  const line = { value: '', words: [] };
+
+function wrap(text, type) {
+  if (type === 1) {
+    return `<div class="tfx"><span>${text}</span></div>`;
+  } else if (type === 2) {
+    return `<span class="word">${text}</span>`;
+  } else if (type === 3) {
+    return `<span class="ltr">${text}</span>`;
+  }
+}
+function check(value, lines, div, width, output, o) {
+  for (let k = 0; k < value.length; k++) {
+    const word = value[k];
+
+    lines.value += word;
+    div.innerHTML = lines.value;
+
+    lines.words.push(word);
+
+    if (div.offsetWidth > width) {
+      lines.words.pop();
+      divide(lines, output, o);
+
+      lines.value = word;
+      lines.words = [word + space];
+    }
+
+    lines.value += space;
+  }
+}
+function newline(obj, div, width, o) {
+  const lines = { value: '', words: [] };
   const output = [];
 
-  for (let i = 0; i < text.length; i++) {
-    const node = text[i];
-
-    for (let k = 0; k < node.value.length; k++) {
-      const key = node.value[k];
-      const prev = line.value.length - 1;
-
-      measure(node, key, line);
-      span.innerHTML = line.value;
-
-      if (span.offsetWidth > width) {
-        const sentance = line.value.substring(0, prev);
-        line.words.pop();
-        output.push({ sentance, words: line.words });
-
-        if (text.length !== 1 || node.value.length !== 1) {
-          k -= 1;
-        }
-
-        line.value = '';
-        line.words = [];
-      }
-    }
-
-    if (i === text.length - 1) {
-      const sentance = line.value.substring(0, line.value.length - 1);
-      output.push({ sentance, words: line.words });
-    }
-  }
-
-  return output;
-}
-function wrapper(text, node, type) {
-  if (type === 1) {
-    return `<div class="line"><div class="sfx">${text}</div></div>`;
-  } else if (type === 2) {
-    let ele = document.createElement('span');
-
-    if (node) {
-      const a = node.cloneNode();
-      a.innerHTML = text;
-      ele.appendChild(a);
+  for (let i = 0; i < obj.length; i++) {
+    const node = obj[i];
+    if (node.type === 3) {
+      check(node.value, lines, div, width, output, o);
     } else {
-      ele.innerHTML = text;
     }
-
-    return `<div class="word" style="display: inline-block;">${ele.innerHTML}</div>`;
-  } else if (type === 3) {
-    return `<div class="letter" style="display: inline-block;">${text}</div>`;
   }
-}
 
-function output(node, arr, o = {}) {
-  let sentance = '';
-
-  for (let i = 0; i < arr.length; i++) {
-    const key = arr[i].words;
-    for (let k = 0; k < key.length; k++) {
-      const word = key[k].word;
-
-      if (o.letters) {
-        let space = word.indexOf('&nbsp;') - 1;
-        let letter = '';
-
-        for (let j = 0; j < word.length; j++) {
-          if (space === j) {
-            letter += wrapper(word.slice(space), '', 3);
-            break;
-          } else {
-            letter += wrapper(word[j], '', 3);
-          }
-        }
-
-        sentance += wrapper(letter, key[k].node, 2);
-      } else {
-        sentance += wrapper(word, key[k].node, 2);
-      }
-    }
-
-    node.innerHTML += wrapper(sentance, '', 1);
-    sentance = '';
-  }
+  divide(lines, output, o);
+  return output;
 }
 
 function split(node, o) {
   let compute = computed(node);
-  let span = document.createElement('div');
+  let div = document.createElement('div');
 
-  init(span, compute);
+  if (o.ltrs) o.words = true;
+  init(div, compute);
 
-  const width = getWidth(node);
-  let text = splitText(node);
+  const width = node.offsetWidth;
+  let obj = splitText(node);
 
-  filter(text);
-  const arr = lines(text, span, width);
+  filter(obj);
 
-  document.body.removeChild(span);
+  const output = newline(obj, div, width, o);
   node.innerHTML = '';
 
-  output(node, arr, o);
+  document.body.removeChild(div);
+  output.map(({ line }) => (node.innerHTML += line));
 
   return {
-    lines: node.querySelectorAll('.sfx'),
-    words: node.querySelectorAll('.word'),
-    letters: node.querySelectorAll('.letter')
+    lines: iSet.sEls(node, '.tfx'),
+    words: iSet.sEls(node, '.word'),
+    ltrs: iSet.sEls(node, '.ltr')
   };
 }
 
