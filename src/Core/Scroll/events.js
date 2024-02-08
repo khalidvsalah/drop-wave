0c8add6aac8c3518b7365073d058a419af15315a
Khalid Salah <khalidsalah1522@gmail.com>
2024-02-08 20:40:20 +0200

    fix draging diraction

--- src/Core/Scroll/events.js
+++ src/Core/Scroll/events.js
@@ -84,23 +84,22 @@ export default class _events {
   }
 
   _event(e) {
-    this.time = e.timeStamp - this.time;
-    this.offset = this.drag;
+    if (e.type == 'wheel' || this.mousedown) {
+      this.time = e.timeStamp - this.time;
+      this.offset = this.drag;
 
-    if (e.type == 'wheel') {
       this.rafCb();
-      this._wheel(e);
-    } else if (this.mousedown) {
-      this.rafCb();
-      this._move(e);
+
+      if (e.type == 'wheel') this._wheel(e);
+      else if (this.mousedown) this._move(e);
+
+      const offset = this.drag - this.offset;
+
+      this.scroll.speed = Math.abs(offset / this.time);
+      this.scroll.dir = Math.sign(offset);
+
+      this.time = e.timeStamp;
     }
-
-    const offset = this.drag - this.offset;
-
-    this.scroll.speed = Math.abs(offset / this.time);
-    this.scroll.dir = Math.sign(offset);
-
-    this.time = e.timeStamp;
   }
 
   _destroy() {
