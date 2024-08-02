17f74c6f4d7dc462360ce1c947f6ac890d7d6b34
Khalid Salah <khalidsalah1522@gmail.com>
2024-08-02 15:34:21 +0300

    refactor onComplete

--- src/Core/tween/tween.js
+++ src/Core/tween/tween.js
@@ -1,6 +1,7 @@
 import TweenBase from './tweenbase';
 import { storage, store } from '../../Utils/states/storage';
 import { query } from '../../Utils/methods/query';
+import { TWEEN_REVERSE } from '../timeline/timeline';
 
 /**
  * Tween options
@@ -50,25 +51,42 @@ export function tween(elements, options = {}) {
   const arr = [];
   const length = nodes.length - 1;
 
+  const actions = {
+    onStart: options.onStart,
+    onUpdate: options.onUpdate,
+    onComplete: options.onComplete
+  };
+
   nodes.forEach((element, i) => {
+    let late = options.late;
+    late += options.space * i;
+
     if (i !== 0) {
-      options.late += options.space * i;
       options.onStart = undefined;
       options.onUpdate = undefined;
-      options.onComplete = undefined;
     }
+
+    if (i !== length) {
+      options.onComplete = undefined;
+    } else {
+      options.onComplete = actions.onComplete;
+    }
+
     if (storage.has(element)) {
       const tweenbase = store(element);
-      arr.push(tweenbase.push(options, 'p'));
+      arr.push(tweenbase.push({ ...options, late }, 'p'));
     } else {
-      arr.push(store(element, new TweenBase(element, options)));
+      arr.push(store(element, new TweenBase(element, { ...options, late })));
     }
   });
 
   return {
-    reverse: () => {
+    reverse: (symbol, time) => {
       arr.forEach((t, i) => {
-        t.push('r', { late: options.space * (length - i) });
+        t.push('r', {
+          late:
+            (symbol === TWEEN_REVERSE ? time : 0) + options.space * (length - i)
+        });
       });
     },
     play: () => {
