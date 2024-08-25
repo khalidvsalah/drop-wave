5da2b6c0c32c8e945b86e12ab995a4cc8e59cf00
Khalid Salah <khalidsalah1522@gmail.com>
2024-08-25 18:31:07 +0300

    fromTo filter

--- src/Utils/props/components/filter.js
+++ src/Utils/props/components/filter.js
@@ -1,3 +1,28 @@
+function _blur(start, end) {
+  const o = {
+    start: parseFloat(start),
+    end: parseFloat(end)
+  };
+  o.lerp = o.end - o.start;
+  return t => `blur(${o.start + o.lerp * t}px)`;
+}
+function _gray(start, end) {
+  const o = {
+    start: parseFloat(start),
+    end: parseFloat(end)
+  };
+  o.lerp = o.end - o.start;
+  return t => `grayscale(${o.start + o.lerp * t}%)`;
+}
+function _contrast(start, end) {
+  const o = {
+    start: parseFloat(start),
+    end: parseFloat(end)
+  };
+  o.lerp = o.end - o.start;
+  return t => `contrast(${o.start + o.lerp * t}%)`;
+}
+
 /**
  * @param {object} p - filter.
  * @param {object} info - computed style.
@@ -14,28 +39,26 @@ function filter(p, { computed }) {
 
   for (const key in p) {
     if (key === 'blur' || blur) {
-      const o = {
-        start: blur ? +blur[1] : 0,
-        end: parseFloat(p.blur)
-      };
-      o.lerp = o.end - o.start;
-      arrs.push(t => `blur(${o.start + o.lerp * t}px)`);
+      const b = p.blur;
+      const bfrom = Array.isArray(b);
+      arrs.push(_blur(bfrom ? b[0] : blur ? +blur[1] : 0, bfrom ? b[1] : b));
     }
     if (key === 'gray' || gray) {
-      const o = {
-        start: (gray ? +gray[1] : 0) * 100,
-        end: parseFloat(p.gray || 0)
-      };
-      o.lerp = o.end - o.start;
-      arrs.push(t => `grayscale(${o.start + o.lerp * t}%)`);
+      const g = p.gray;
+      const gfrom = Array.isArray(g);
+      arrs.push(
+        _gray(gfrom ? g[0] : (gray ? +gray[1] : 0) * 100, gfrom ? g[1] : g)
+      );
     }
     if (key === 'contrast' || contrast) {
-      const o = {
-        start: (contrast ? +contrast[1] : 0) * 100,
-        end: parseFloat(p.contrast || 0)
-      };
-      o.lerp = o.end - o.start;
-      arrs.push(t => `contrast(${o.start + o.lerp * t}%)`);
+      const c = p.contrast;
+      const cfrom = Array.isArray(c);
+      arrs.push(
+        _contrast(
+          cfrom ? c[0] : (contrast ? +contrast[1] : 0) * 100,
+          cfrom ? c[1] : c
+        )
+      );
     }
   }
 
