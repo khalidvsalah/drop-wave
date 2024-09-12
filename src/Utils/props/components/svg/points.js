760e7d71feb6b057784d88153e81b07e5aecd27b
Khalid Salah <khalidsalah1522@gmail.com>
2024-09-12 15:59:12 +0300

    add points
    morph svg points

---
+++ src/Utils/props/components/svg/points.js
@@ -0,0 +1,39 @@
+import { lerp } from '../../../../index';
+
+/**
+ * @param {string} pointString
+ * @returns {(number|string)[]} - An array of numbers and strings parsed from the input string.
+ */
+const parsePoints = (pointString) => {
+  const result = [];
+  const pointPairs = pointString.split(' ');
+
+  for (const pair of pointPairs) {
+    const coordinates = pair.split(',');
+    for (const coord of coordinates) {
+      result.push(isNaN(coord) ? coord : +coord);
+    }
+  }
+
+  return result;
+};
+
+/**
+ * Creates an interpolating function between two sets of point coordinates.
+ * @param {string} p
+ * @param {object} info
+ * @returns {Function}
+ */
+const points = (p, { element }) => {
+  const startPoints = parsePoints(element.getAttribute('points'));
+  const endPoints = parsePoints(p);
+
+  return (t) => {
+    return startPoints
+      .map((point, i) => lerp(point, endPoints[i], t))
+      .join(' ');
+  };
+};
+
+const setValue = (element) => (value) => element.setAttribute('points', value);
+export default { callback: points, setValue };
