04bfe8cb98623ce2c57fa26d7d00d9d0d2b3dba3
Khalid Salah <khalidsalah1522@gmail.com>
2024-10-08 00:52:54 +0300

    fix visiability bug

--- src/Utils/raf/visiability.js
+++ src/Utils/raf/visiability.js
@@ -2,7 +2,7 @@ import { states } from '../states/states';
 
 class Visiability {
   init(raf) {
-    window.onvisibilitychange = states.create('visibilitychange').notify;
+    document.onvisibilitychange = states.create('visibilitychange').notify;
 
     if (!this.raf) {
       states.subscribe('visibilitychange', this.change.bind(this));
@@ -15,7 +15,7 @@ class Visiability {
     if (document.hidden) {
       this.hide = now;
     } else {
-      this.raf.items.map(item => {
+      this.raf.items.map((item) => {
         item.st += now - this.hide;
       });
     }
