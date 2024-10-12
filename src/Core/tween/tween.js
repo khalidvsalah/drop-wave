cdf5a6847f59d43685cf303e3eb1c244fc5edb40
Khalid Salah <khalidsalah1522@gmail.com>
2024-10-12 15:35:40 +0300

    fix a bug
    push class that extends array to a new array

--- src/Core/tween/tween.js
+++ src/Core/tween/tween.js
@@ -10,93 +10,98 @@ import { REVERSE } from '../timeline/timeline';
  * @returns {import('../../types/tweenTypes.js').TWEEN_CONTROLLERS}
  */
 export function tween(elements, options = {}) {
-  const tweens = [];
-  let nodes;
+  try {
+    const tweens = [];
+    const nodes = [];
 
-  options = {
-    space: 0,
-    delay: 0,
-    duration: 1,
-    ease: 'linear',
-    ...options,
-  };
+    options = {
+      space: 0,
+      delay: 0,
+      duration: 1,
+      ease: 'linear',
+      ...options,
+    };
 
-  if (typeof elements === 'string') {
-    nodes = [...document.querySelectorAll(elements)];
-  } else if (elements instanceof NodeList) {
-    nodes = [...elements];
-  } else if (Array.isArray(elements)) {
-    if (typeof elements[0] === 'number') {
-      nodes = elements;
+    if (typeof elements === 'string') {
+      nodes.push(...document.querySelectorAll(elements));
+    } else if (elements instanceof NodeList) {
+      nodes.push(...elements);
+    } else if (Array.isArray(elements)) {
+      if (typeof elements[0] === 'number') {
+        nodes.push(elements);
+      } else {
+        nodes.push(...elements);
+      }
     } else {
-      nodes = elements;
+      nodes.push(elements);
     }
-  } else {
-    nodes = [elements];
+
+    const length = nodes.length - 1;
+
+    const actions = {
+      onStart: options.onStart,
+      onUpdate: options.onUpdate,
+      onComplete: options.onComplete,
+    };
+
+    /**
+     * Create a new tween instance for each element.
+     * When reusing the same element, the same instance is reused.
+     */
+    const tweenNodes = nodes.map((element, i) => {
+      let delay = options.delay;
+      delay += options.space * i;
+
+      if (i !== 0) {
+        options.onStart = undefined;
+        options.onUpdate = undefined;
+      }
+
+      if (i !== length) {
+        options.onComplete = undefined;
+      } else {
+        options.onComplete = actions.onComplete;
+      }
+
+      const newOptions = { ...options, delay };
+      if (storage.has(element)) {
+        const tweenbase = store(element);
+        tweenbase.push('play', newOptions);
+        tweens.push(tweenbase);
+      } else {
+        const tween = new TweenBase(element, newOptions);
+        tweens.push(store(element, tween));
+      }
+
+      return newOptions;
+    });
+
+    return {
+      reverse: (symbol, time) => {
+        tweens.forEach((tween, idx) => {
+          const isTimeline = symbol === REVERSE;
+          const delay =
+            (isTimeline ? time : 0) + options.space * (length - idx);
+          tween.push('reverse', { delay });
+        });
+      },
+      play: () => {
+        tweens.forEach((tween, idx) => {
+          tween.push('play', tweenNodes[idx]);
+        });
+      },
+      pause: () => {
+        tweens.forEach((tween) => {
+          tween.stop();
+        });
+      },
+      kill: () => {
+        tweens.forEach((tween) => {
+          tween.delay.destroy();
+        });
+      },
+    };
+  } catch (e) {
+    throw TypeError('Element(s) not found: ' + elements);
   }
-
-  const length = nodes.length - 1;
-
-  const actions = {
-    onStart: options.onStart,
-    onUpdate: options.onUpdate,
-    onComplete: options.onComplete,
-  };
-
-  /**
-   * Create a new tween instance for each element.
-   * When reusing the same element, the same instance is reused.
-   */
-  nodes = nodes.map((element, i) => {
-    let delay = options.delay;
-    delay += options.space * i;
-
-    if (i !== 0) {
-      options.onStart = undefined;
-      options.onUpdate = undefined;
-    }
-
-    if (i !== length) {
-      options.onComplete = undefined;
-    } else {
-      options.onComplete = actions.onComplete;
-    }
-
-    const newOptions = { ...options, delay };
-    if (storage.has(element)) {
-      const tweenbase = store(element);
-      tweenbase.push('play', newOptions);
-      tweens.push(tweenbase);
-    } else {
-      const tween = new TweenBase(element, newOptions);
-      tweens.push(store(element, tween));
-    }
-
-    return newOptions;
-  });
-
-  return {
-    reverse: (symbol, time) => {
-      tweens.forEach((tween, idx) => {
-        const isTimeline = symbol === REVERSE;
-        const delay = (isTimeline ? time : 0) + options.space * (length - idx);
-        tween.push('reverse', { delay });
-      });
-    },
-    play: () => {
-      tweens.forEach((tween, idx) => {
-        tween.push('play', nodes[idx]);
-      });
-    },
-    pause: () => {
-      tweens.forEach((tween) => {
-        tween.stop();
-      });
-    },
-    kill: () => {
-      tweens.forEach((tween) => {
-        tween.delay.destroy();
-      });
-    },
-  };
 }
