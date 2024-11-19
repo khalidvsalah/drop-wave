13cff2002b8c4f70dd1f16227d73f7736b04e0b6
Khalid Salah <khalidsalah1522@gmail.com>
2024-11-19 21:09:19 +0200

    update selector

--- src/helpers/selector.js
+++ src/helpers/selector.js
@@ -1,19 +1,30 @@
-export default function selector(elements) {
-  const nodes = [];
+/**
+ * selector
+ *
+ * @param {any} elements target(s) or string selector
+ * @param {Blob} obj select object also
+ * @returns {Element | Element[] | null}
+ */
 
-  try {
-    if (typeof elements === 'string') {
-      nodes.push(...document.querySelectorAll(elements));
-      if (nodes.length === 0) throw new Error();
-    } else if (elements instanceof NodeList) {
-      nodes.push(...elements);
-    } else if (Array.isArray(elements)) {
-      if (typeof elements[0] === 'number') nodes.push(elements);
-      else nodes.push(...elements);
-    } else nodes.push(elements);
-
-    return nodes;
-  } catch (e) {
-    throw TypeError('Element(s) not found: ' + elements);
+export default function selector(elements, obj) {
+  if (typeof elements === 'string') {
+    return document.querySelectorAll(elements);
+  } else if (
+    elements instanceof NodeList ||
+    elements instanceof HTMLCollection
+  ) {
+    return elements;
+  } else if (elements instanceof Array) {
+    if (obj && elements.every((element) => typeof element === 'number')) {
+      return elements;
+    } else {
+      return elements;
+    }
+  } else if (elements instanceof Node) {
+    return [elements];
+  } else if (obj && typeof elements === 'object' && elements !== null) {
+    return [elements];
   }
+
+  console.warn(`Drop-Wave target element ${elements} not found.`);
 }
