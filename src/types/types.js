8c1e445f0e43d64e5b3e0c5563882d706800b70c
Khalid Salah <khalidsalah1522@gmail.com>
2024-11-19 21:06:40 +0200

    update types & and new types

--- src/types/types.js
+++ src/types/types.js
@@ -1,21 +1,21 @@
-/** * @typedef {Node|HTMLElement|String} ELEMENT */
-/** * @typedef {NodeList|Node|HTMLElement|Array} NODES */
+/** * @typedef {NodeList|Node|HTMLElement|Array} ELEMENTS */
 /** * @typedef {string|Array<string>} STRING_ARR */
 /** * @typedef {string|Array<number>} NUMBER_ARR */
 
 /* Start Properties */
-/** * @typedef {{x:STRING_ARR, y:STRING_ARR, scale:NUMBER_ARR, scaleX:NUMBER_ARR, scaleY:NUMBER_ARR}} TRANSFORM */
-/** * @typedef {{circle:FROM_TO, polygon:FROM_TO}} CLIP_PATH */
+/** * @typedef {{x:STRING_ARR, y:STRING_ARR, scale:NUMBER_ARR, scaleX:NUMBER_ARR, scaleY:NUMBER_ARR}} transformType */
+/** * @typedef {{circle:FROM_TO, polygon:FROM_TO}} clipPathType */
 /* End Properties */
 
 /**
- * @typedef {Object} PROPERTIES
+ * Tweened Properties
+ * @typedef {Object} propertiesType
  * @property {number} [opacity]
  * @property {number} [alpha]
- * @property {TRANSFORM} [transform]
- * @property {TRANSFORM} [move]
- * @property {CLIP_PATH} [clip]
- * @property {CLIP_PATH} [clipPath]
+ * @property {transformType} [transform]
+ * @property {transformType} [move]
+ * @property {clipPathType} [clip]
+ * @property {clipPathType} [clipPath]
  * @property {{gray:FROM_TO, blur:FROM_TO, contrast:FROM_TO}} [filter]
  * @property {NUMBER_ARR} [draw]
  * @property {string} [points]
@@ -23,19 +23,21 @@
  */
 
 /**
- * @typedef {Object} TWEEN_OPTIONS
+ * Tween Options
+ * @typedef {Object} tweenOptionsType
  * @property {number} [duration]
  * @property {number} [delay]
  * @property {number} [space]
  * @property {string} [ease]
- * @property {PROPERTIES} [props]
+ * @property {propertiesType} [props]
  * @property {Function} [onStart]
  * @property {Function} [onUpdate]
  * @property {Function} [onComplete]
  */
 
 /**
- * @typedef {Object} TWEEN_CONTROLLERS
+ * Tween returned Object
+ * @typedef {Object} tweenReturnsType
  * @property {Function} reverse
  * @property {Function} play
  * @property {Function} pause
@@ -43,7 +45,8 @@
  */
 
 /**
- * @typedef {Object} SCROLL_OPTIONS
+ * Virtual Scroll Options
+ * @typedef {Object} scrollOptionsType
  * @property {string} [name]
  * @property {HTMLElement} [container]
  * @property {'x'|'y'} [dir]
@@ -56,13 +59,20 @@
 
 /**
  * Trigger options
- * @typedef {Object} TRIGGER_OPTIONS
- * @property {NODES} [trigger]
- * @property {PROPERTIES} [animate]
- * @property {TWEEN_OPTIONS} [tween]
+ * @typedef {Object} triggerOptionsType
+ * @property {ELEMENTS} [trigger]
+ * @property {propertiesType} [animate]
+ * @property {tweenOptionsType} [tween]
  * @property {string} [ease]
  * @property {string|number|Function} [start]
  * @property {string|number|Function} [end]
  * @property {{start:string|number|Function, end:start|number|Function}} [pin]
  * @property {Function} [onUpdate]
+ */
+
+/**
+ * Timeline options
+ * @typedef {Object} timelineOptionsType
+ * @property {number} [delay]
+ * @property {string|symbol} [name]
  */
