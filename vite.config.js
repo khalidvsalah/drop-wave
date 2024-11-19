a58daa557ad429f03058cdcb9d2482e08e5ecce5
Khalid Salah <khalidsalah1522@gmail.com>
2024-11-19 21:09:54 +0200

    host on network for mobile testing

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '__test__/browser',
-  server: { port: 1234 },
+  server: { port: 1234, host: true },
   build: { outDir: './dist' },
 });
