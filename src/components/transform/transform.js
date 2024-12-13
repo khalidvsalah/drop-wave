4795e6f8053b065776f7785e53b52d59d0fc1e47
khalidvsalah <khalidvsalah@gmail.com>
2024-12-13 18:19:14 +0200

    optimize transfrom component

--- src/components/transform/transform.js
+++ src/components/transform/transform.js
@@ -20,7 +20,6 @@ const parseTransform = (value) => {
   if (value.scale != null) {
     [value.scaleX, value.scaleY] = [value.scale, value.scale];
   }
-
   if (value.rotate != null) {
     value.rotateZ = value.rotate;
   }
@@ -33,12 +32,55 @@ const parseTransform = (value) => {
       const matrix = /\((.*)\)/.exec(value);
       value = isMatrix3d ? matrix3d(matrix) : matrix2d(matrix);
     }
+  } else {
+    value = {
+      translate: { x: value.x, y: value.y },
+      scale: { x: value.scaleX, y: value.scaleY },
+      rotate: { x: value.rotateX, y: value.rotateY, z: value.rotateZ },
+    };
   }
 
+  return value;
+};
+
+/**
+ * Return the first defined value.
+ * @param {number|string} values
+ * @returns {number|string}
+ */
+const isNumber = (o1, o2, o3) => {
+  return o1 != null ? o1 : o2 != null ? o2 : o3;
+};
+
+/**
+ * Fill undefined values.
+ *
+ * @param {string} value
+ * @returns {object}
+ */
+const fill = (value, fillValue) => {
   return {
-    translate: { x: value.x, y: value.y },
-    scale: { x: value.scaleX, y: value.scaleY },
-    rotate: { x: value.rotateX, y: value.rotateY, z: value.rotateZ },
+    translate: {
+      x: isNumber(
+        value.translate.x,
+        fillValue.translate.x,
+        defaults.translate.x
+      ),
+      y: isNumber(
+        value.translate.y,
+        fillValue.translate.y,
+        defaults.translate.y
+      ),
+    },
+    scale: {
+      x: isNumber(value.scale.x, fillValue.scale.x, defaults.scale.x),
+      y: isNumber(value.scale.y, fillValue.scale.y, defaults.scale.y),
+    },
+    rotate: {
+      x: isNumber(value.rotate.x, fillValue.rotate.x, defaults.rotate.x),
+      y: isNumber(value.rotate.y, fillValue.rotate.y, defaults.rotate.y),
+      z: isNumber(value.rotate.z, fillValue.rotate.z, defaults.rotate.z),
+    },
   };
 };
 
@@ -56,13 +98,16 @@ const transform = (endValue, { element, computed }) => {
   startValue = parseTransform(startValue);
   endValue = parseTransform(endValue);
 
+  startValue = fill(startValue, endValue);
+  endValue = fill(endValue, startValue);
+
   const arr = [];
 
   if (
-    endValue.translate.x ||
-    endValue.translate.y ||
-    startValue.translate.x ||
-    startValue.translate.y
+    endValue.translate.x != null ||
+    endValue.translate.y != null ||
+    startValue.translate.x != null ||
+    startValue.translate.y != null
   ) {
     arr.push(
       _translate(
@@ -74,10 +119,10 @@ const transform = (endValue, { element, computed }) => {
   }
 
   if (
-    endValue.scale.x ||
-    endValue.scale.y ||
-    startValue.scale.x ||
-    startValue.scale.y
+    endValue.scale.x != 1 ||
+    endValue.scale.y != 1 ||
+    startValue.scale.x != 1 ||
+    startValue.scale.y != 1
   ) {
     arr.push(
       _scale(
@@ -88,12 +133,12 @@ const transform = (endValue, { element, computed }) => {
   }
 
   if (
-    endValue.rotate.x ||
-    endValue.rotate.y ||
-    endValue.rotate.z ||
-    startValue.rotate.x ||
-    startValue.rotate.y ||
-    startValue.rotate.z
+    endValue.rotate.x != 0 ||
+    endValue.rotate.y != 0 ||
+    endValue.rotate.z != 0 ||
+    startValue.rotate.x != 0 ||
+    startValue.rotate.y != 0 ||
+    startValue.rotate.z != 0
   ) {
     arr.push(
       _rotate(
